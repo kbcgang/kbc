5ed7bf8f01104e76c1ec9977b81aa2f488c1cb8c
kbcgang <kyphaply81@gmail.com>
2024-11-25 23:32:18 +0700

    sang

--- HTML/coding.cpp
+++ HTML/coding.cpp
@@ -1,21 +1,72 @@
-///Sức mạnh của lòng kiêu hãnh --- trungkien1252010@gmail.com ----
+/// Sức mạnh của lòng kiêu hãnh --- trungkien1252010@gmail.com ----
 #include <bits/stdc++.h>
 using namespace std;
 #define kien long long
 #define Million 1000000
-#define NT 10000000
 #define MOD 1000000007
-kien n,k,m,dem,f[1000000], a[1000000];
-kien maxx,minn, vtr,ans,l,r, dp[1000000];
+
+kien n, k, t, f[Million], dp[Million], dp1[Million];
+
+// Hàm đảo ngược số
+int daoso(int n)
+{
+    int reversed = 0;
+    while(n > 0)
+    {
+        reversed = reversed * 10 + (n % 10);
+        n /= 10;
+    }
+    return reversed;
+}
+
+// Hàm sàng số nguyên tố
+void sangNT()
+{
+    for(int i = 2; i < Million; i++)
+    {
+        f[i] = 1;
+    }
+    f[0] = f[1] = 0; // 0 và 1 không phải số nguyên tố
+    for(int i = 2; i * i < Million; i++)
+    {
+        if(f[i])
+        {
+            for(int j = i * i; j < Million; j += i)
+            {
+                f[j] = 0;
+            }
+        }
+    }
+}
 
 int main()
 {
-    ios_base::sync_with_stdio(0);
+    ios_base::sync_with_stdio(false);
     cin.tie(0);
-    cout.tie(0);
-    cin >> n;
-    for (int i = 1; i <= n; i++)
+    // Khởi tạo
+    sangNT();
+    dp[0] = dp1[0] = 0;
+    // Tính các mảng dp và dp1
+    for(int i = 1; i < Million; i++)
     {
-        
+        dp[i] = dp[i - 1];
+        dp1[i] = dp1[i - 1];
+        if(f[i])
+        {
+            dp1[i] += i; // Tổng các số nguyên tố
+            if(f[daoso(i)])
+            {
+                dp[i]++; // Đếm số nguyên tố có số đảo ngược cũng là nguyên tố
+            }
+        }
     }
+    // Xử lý truy vấn
+    cin >> t;
+    while(t--)
+    {
+        cin >> n >> k;
+        cout << dp1[k] - dp1[n - 1] << " " // Tổng các số nguyên tố từ n đến k
+             << dp[k] - dp[n - 1] << "\n"; // Số lượng số nguyên tố đặc biệt
+    }
+    return 0;
 }
--- HTML/find.cpp
+++ HTML/find.cpp
@@ -5,40 +5,19 @@ using namespace std;
 #define Million 1000000
 #define NT 10000000
 #define MOD 1000000007
-string s,s1,x,x1;
-kien sum, sum1;
+#define RUN for (int i = 1; i <= n; i++)
+kien n,k,m,dem,f[1000000], a[1000000];
+kien maxx,minn, vtr,ans,l,r, dp[1000000];
 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    getline(cin , x);
-    getline (cin , x1);
-    for (int i = 0; i < x.size(); i++)
+    cin >> n >> k;
+    for (int i = 1; i <= n; i++)
     {
-        if (x[i] >= '0' and x[i] <= '9')
-        {
-            s += x[i];
-        }
+        cin >> a[i];
     }
-    for (int i = 0; i < x1.size(); i++)
-    {
-        if (x1[i] >= '0' and x1[i] <= '9')
-        {
-            s1 += x1[i];
-        }
-    }
-    if (s.size() < s1.size()) swap(s, s1);
-    int tam = 0;
-    string ans = "";
-    for (int i = s.size() - 1, j = s1.size() - 1; i >= 0 || tam; i--, j--) {
-        int num_a = (i >= 0) ? (s[i] - '0') : 0;
-        int num_b = (j >= 0) ? (s1[j] - '0') : 0;
-        int sum = num_a + num_b + tam;
-        tam = sum / 10;
-        ans = char(sum % 10 + '0') + ans;
-    }
-
-    cout << ans << endl;
+    
 }
